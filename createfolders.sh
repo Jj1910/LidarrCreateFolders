eacec14663f6f4b64391dd024139758f23470c7a
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2025-03-07 11:17:23 -0500

    Create createfolders.sh

---
+++ createfolders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Url=""
+APIKey=""
+ContainerPath=""
+TargetPath=""
+
+artists=$(curl --silent $Url/artist -H "accept: application/json" -H "X-Api-Key: $APIKey")
+echo "$artists" | jq -cr '.[].path' | while read -r artistpath; do
+        path=$(sed -E "s|$ContainerPath|$TargetPath|g" <<< $artistpath)
+        echo Creating $path...
+        mkdir "$path"
+done
