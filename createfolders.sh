a6a19894a80a8358c3d5ddeec5d91565019434d0
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2025-03-08 06:57:47 -0500

    Adding Check for Existing Directories

--- createfolders.sh
+++ createfolders.sh
@@ -8,6 +8,10 @@ TargetPath=""
 artists=$(curl --silent $Url/artist -H "accept: application/json" -H "X-Api-Key: $APIKey")
 echo "$artists" | jq -cr '.[].path' | while read -r artistpath; do
         path=$(sed -E "s|$ContainerPath|$TargetPath|g" <<< $artistpath)
-        echo Creating $path...
-        mkdir -p "$path"
+        if [ ! -d "$path" ]; then
+                echo Creating $path...
+                mkdir "$path"
+        else
+                echo $path Already Exists!
+        fi
 done
